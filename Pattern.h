1b67610587af050716883c2a0423ee28697a504d
Jianqiao Guo <jgu56@cs19090bs.canterbury.ac.nz>
2019-10-11 14:34:36 +1300

    initialise the pattern

---
+++ Pattern.h
@@ -0,0 +1,16 @@
+#ifndef PATTERN_H 
+#define PATTERN_H
+
+
+#include "system.h"
+
+//initialise the paper pattern
+static const uint8_t Paper[];
+
+//initialise the rock pattern
+static const uint8_t Rock[];
+
+//initialise the scissor pattern
+static const uint8_t Scissor[];
+
+#endif
