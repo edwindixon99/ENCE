9401f49de0d827acbe0ae08776a347e2e03ceb84
Jianqiao Guo <jgu56@cs19098bs.canterbury.ac.nz>
2019-10-13 17:50:43 +1300

    pattern of rock paper scissor

--- Pattern.c
+++ Pattern.c
@@ -1,26 +1,38 @@
 #include "pio.h"
 #include "Pattern.h"
+#include "tinygl.h"
+#include "pacer.h"
 
 //Paper pattern
 static const uint8_t Paper[] =
 {
-    0b1111111,
-    0b1000001,
-    0b1000001,
-    0b1000001,
-    0b1111111,
+	tinygl_init(1000);
+	pacer_init(1000);
+	pacer_wait();
+	
+	tinygl_draw_line (tinygl_point(0,0), tinygl_point(0,6),7);
+    tinygl_draw_line (tinygl_point(0,0), tinygl_point(4,0),5);
+    tinygl_draw_line (tinygl_point(4,0), tinygl_point(4,6),7);
+    tinygl_draw_line (tinygl_point(4,6), tinygl_point(0,6),5);
+    
+    tinygl_update();
 };
 
 //Rock pattern
-static const uint8_t Rock[] =
+static void Rock() 
 {
-    0b0001110,
-    0b1110001,
-    0b0000001,
-    0b1110001,
-    0b0001110,
-};
-
+	tinygl_init(1000);
+	pacer_init(1000);
+	pacer_wait();
+	
+	tinygl_draw_line (tinygl_point(0,1), tinygl_point(0,3),4);
+    tinygl_draw_line (tinygl_point(1,0), tinygl_point(3,0),5);
+    tinygl_draw_line (tinygl_point(4,1), tinygl_point(4,3),4);
+    tinygl_draw_line (tinygl_point(1,4), tinygl_point(1,6),3);
+    tinygl_draw_line (tinygl_point(3,4), tinygl_point(3,6),3);
+    
+    tinygl_update();
+}
 //Scissor pattern
 static const uint8_t Scissor[] =
 {
