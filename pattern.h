2aba013bb62d726f7003ab44e5d1cd9928c6fbe4
Jianqiao Guo <jgu56@cs18223kq.canterbury.ac.nz>
2019-10-15 13:30:25 +1300

    initialise patterns

---
+++ pattern.h
@@ -0,0 +1,12 @@
+#ifndef PATTERN_H 
+#define PATTERN_H
+
+//initialise the paper pattern
+void Rock(void);
+
+//initialise the rock pattern
+void Paper(void);
+
+//initialise the scissor pattern
+void Scissor(void);
+#endif
