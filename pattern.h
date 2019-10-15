5360a57fec7697c980875f7a39142173f9c50fc6
Edwin James Dixon <ejd83@cs17066jp.canterbury.ac.nz>
2019-10-15 20:30:37 +1300

    added comment

--- pattern.h
+++ pattern.h
@@ -1,4 +1,11 @@
-#ifndef PATTERN_H 
+/** @file   pattern.c
+    @author Team 425 Edwin Dixon ejd83 & Jianqiao Guo jgu56
+    @date    October 2019
+
+        Header file for pattern.c
+*/
+
+#ifndef PATTERN_H
 #define PATTERN_H
 
 //initialise the paper pattern
