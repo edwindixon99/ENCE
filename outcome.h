7002a69ba083d79ff063999fd93c44202817cfa2
Edwin James Dixon <ejd83@cs17066jp.canterbury.ac.nz>
2019-10-15 20:31:10 +1300

    added comment and functions

--- outcome.h
+++ outcome.h
@@ -1,11 +1,23 @@
+/** @file   pattern.c
+    @author Team 425 Edwin Dixon ejd83 & Jianqiao Guo jgu56
+    @date    October 2019
+
+        Header file for outcome.c
+*/
+
 #ifndef OUTCOME_H
 #define OUTCOME_H
+
+
+void game_init(void);
 
 char* show_result (char sender, char reciever);
 
 char update_character (char character);
 
 void display_pattern (char character);
+
+void end_screen(char playing);
 
 
 #endif
