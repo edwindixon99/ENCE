a76e3bc151578c7b4aae6780dc3228846104c7a4
Jianqiao Guo <jgu56@cs18223kq.canterbury.ac.nz>
2019-10-15 16:01:44 +1300

    add led function

--- outcome.c
+++ outcome.c
@@ -2,26 +2,33 @@
 #include "navswitch.h"
 #include "tinygl.h"
 #include "pattern.h"
+#include "led.h"
 
 /** returns a string which is to be displayed on the users display based on the outcome of the game.
     @param  the character representing the pattern the user has recieved from other player.
     @param  the character representing the pattern the user has chosen.
     @return a string which is the outcome of the game.  */
 char* show_result (char reciever, char sender)
-{
+{	
     char* outcome = "";
 
     // draw case. displays DRAW
     if (sender == reciever) {
         outcome = "DRAW";
+        led_init();
+        led_set(LED1, 0);
 
     // possible ways for user to win. displays WIN
     } else if ((sender == 'R' && reciever == 'P') || (sender == 'S' && reciever == 'R') || (sender == 'P' && reciever == 'S')) {
         outcome = "WIN";
+        led_init();
+        led_set(LED1, 1);
 
     // possible ways for user to lose. displays LOSE
     } else if ((sender == 'P' && reciever == 'R') || (sender == 'R' && reciever == 'S') || (sender == 'S' && reciever == 'P')) {
         outcome = "LOSE";
+        led_init();
+        led_set(LED1, 0);
 
     // unexpected character has been recieved
     } else {
