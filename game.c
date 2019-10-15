a5f74c8912b0bf3ea6f5d42e7379ff787e8fa82a
Edwin James Dixon <ejd83@cs17066jp.canterbury.ac.nz>
2019-10-15 20:29:58 +1300

    modularized

--- game.c
+++ game.c
@@ -1,3 +1,11 @@
+/** @file   game.c
+    @author Team 425 Edwin Dixon ejd83 & Jianqiao Guo jgu56
+    @date    October 2019
+
+    Rock, Paper, Scissors. Main game module.
+*/
+
+
 #include "system.h"
 #include "pacer.h"
 #include "navswitch.h"
@@ -9,49 +17,30 @@
 #include "pattern.h"
 
 
-#define PACER_RATE 500
+#define TINYGL_RATE 500
+#define PACER_RATE 2000
 #define MESSAGE_RATE 3
 
-
-void display_character (char character)
-{
-    char buffer[2];
-    buffer[0] = character;
-    buffer[1] = '\0';
-    tinygl_text (buffer);
-}
 
 
 int main (void)
 {
-    int count;
     int start_game = 0;
     char playing = 0;
     char character = 'P';
     char otherplayer_selected = 0;
-
-    system_init ();
-    tinygl_init (PACER_RATE);
-    tinygl_font_set (&font5x7_1);
-    tinygl_text_speed_set (MESSAGE_RATE);
-    button_init ();
+    char* result = "";
 
 
-    tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
-    navswitch_init ();
-
-    /* TODO: Initialise IR driver.  */
-    ir_uart_init ();
-
-    pacer_init (2000);
+    game_init();
 
     tinygl_text("Welcome\0");
 
     while (1)
     {
         pacer_wait ();
-        navswitch_update ();
         button_update();
+        navswitch_update ();
 
         if (start_game == 0) {
             tinygl_update ();
@@ -59,9 +48,8 @@ int main (void)
                 start_game = 1;
             }
 
-
         } else if (!(playing != 0 && otherplayer_selected != 0)) {
-            tinygl_update ();
+
             display_pattern (character);
 
             character = update_character (character);
@@ -75,31 +63,17 @@ int main (void)
             }
 
         } else {
-            tinygl_clear();
-            tinygl_text_mode_set (TINYGL_TEXT_MODE_STEP);
-            count = 0;
-            tinygl_text("321\0");
-            while (count < 6000) {
-                count++;
-                pacer_wait ();
-                tinygl_update ();
-            }
-            tinygl_clear();
-            count = 0;
-            while (count < 5000) {
-                pacer_wait ();
-                count++;
-                display_pattern (playing);
-            }
 
-            tinygl_init (PACER_RATE);
-            tinygl_text_speed_set (MESSAGE_RATE);
-            tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
-            tinygl_text(show_result(playing, otherplayer_selected));
+            end_screen(playing);
+            result = show_result(playing, otherplayer_selected);
+            tinygl_text(result);
+
             while (1) {
+
                 pacer_wait ();
-                tinygl_update ();
                 button_update();
+                tinygl_update ();
+
                 if (button_push_event_p (0)) {
                     playing = 0;
                     otherplayer_selected = 0;
